59e5b8092269c507683421a9bc703e2be109ac92
HazelJiang <jintongjiang@gatech.edu>
2019-09-20 17:50:41 -0400

    add allocate points

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=1)])
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
-    selectDifficulty = SelectField("Choose difficulty", choices=[("easy","Easy"), ("medium","Medium"), ("hard","Hard")])
+    selectDifficulty = SelectField("Choose difficulty", choices=[("easy","Easy, you have 16 points to allocate"), ("medium","Medium, you have 12 points to allocate"), ("hard","Hard, you have 8 points to allocate")])
     allocatePilot = StringField("Pilot Skill Points", validators=[DataRequired(), Length(min=1)])
     allocateFighter = StringField("Fighter Skill Points", validators=[DataRequired(), Length(min=1)])
     allocateMerchant = StringField("Merchant Skill Points", validators=[DataRequired(), Length(min=1)])
