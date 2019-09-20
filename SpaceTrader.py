59f343d0d8e9f509f7161f87a6eaa0840f835603
Hanzhong Kang <kanghanzhong@gmail.com>
2019-09-20 19:57:23 -0400

    update

--- SpaceTrader.py
+++ SpaceTrader.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, redirect
 from forms import RegistrationForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cd291fd59e221b9a3a13c13b3b5dbc9e'
 
